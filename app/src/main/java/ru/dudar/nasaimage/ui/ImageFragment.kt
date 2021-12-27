c5cc01cc18a08a5580cf91ac04f57b728b8b2a65
pakad <pakad@mail.ru>
2021-12-27 17:04:07 +0300

    ViewPager

--- app/src/main/java/ru/dudar/nasaimage/ui/ImageFragment.kt
+++ app/src/main/java/ru/dudar/nasaimage/ui/ImageFragment.kt
@@ -1,8 +1,11 @@
 package ru.dudar.nasaimage.ui
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
+import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -28,9 +31,19 @@ class ImageFragment : Fragment(R.layout.image_fragment) {
         super.onViewCreated(view, savedInstanceState)
         _binding = ImageFragmentBinding.bind(view)
 
-        setBottomSheetBehavior(view.findViewById(R.id.bottom_sheet_container))
+        bottomSheetBehavior = BottomSheetBehavior.from(view.findViewById(R.id.bottom_sheet_container))
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+
+
         val textBottomHeader = view.findViewById<TextView>(R.id.bottom_sheet_description_header)
         val textBottom = view.findViewById<TextView>(R.id.bottom_sheet_description)
+
+               
+        binding.textTIL.setEndIconOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW).apply {
+                data = Uri.parse("https://en.wikipedia.org/wiki/${binding.inputText.text.toString()}")
+            })
+        }
 
         viewModel.imageVM.observe(this, Observer {
             binding.nasaIV.load(it.url)
@@ -63,10 +76,7 @@ class ImageFragment : Fragment(R.layout.image_fragment) {
         }
     }
 
-    private fun setBottomSheetBehavior(bottomSheet: ConstraintLayout) {
-        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
-        bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
-    }
+
 
     override fun onDestroyView() {
         _binding = null
